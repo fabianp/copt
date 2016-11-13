de6eacac6fc784ea108c1e91dedb9bd68e88e837
Fabian Pedregosa <f@bianp.net>
2016-11-13 12:02:17 +0100

    initial import website

---
+++ _downloads/example_logistic.py
@@ -0,0 +1,35 @@
+"""
+L1-regularized logistic regression
+==================================
+
+Implementation of L1-regularized logistic regression
+using copt.
+"""
+import numpy as np
+from sklearn.linear_model import logistic
+from copt import proximal_gradient
+
+n_samples, n_features = 100, 10
+X = np.random.randn(n_samples, n_features)
+y = np.random.randn(n_samples)
+alpha = 1.
+
+
+def logloss(x):
+    return logistic._logistic_loss(x, X, y, 1.)
+
+
+def fprime_logloss(x):
+    return logistic._logistic_loss_and_grad(x, X, y, 1.)[1]
+
+
+def g_prox(x, step_size):
+    """
+    prox of alpha * l1
+    """
+    return np.fmax(x - step_size * alpha, 0) - \
+        np.fmax(- x - step_size * alpha, 0)
+
+
+out = proximal_gradient(logloss, fprime_logloss, g_prox, np.zeros(n_features))
+print('Solution', out)
